c058163177f92c5ac7c9fa290a0a3ec476f17a21
bmiranda-lir <78102866+bmiranda-lir@users.noreply.github.com>
2021-02-15 20:42:11 -0600

    Added discord env variable for heroku deployment (#2)
    * added git config to git ignore file
    
    * Added discord token as environment variable.

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
 from discord.ext import commands
+import os
 
 client = commands.Bot(command_prefix="./")
+
 
 @client.event
 async def on_ready():
     print("Bot is ready")
 
-client.run('')
+
+client.run(os.getenv("DISCORD_TOKEN"))
